950d6ddf4209fc899937ce2a7940135efb75dddb
Jo Burgard <mail@joburgard.com>
2023-08-05 17:09:00 +0200

    Start building theme editor

---
+++ src/lib/stories/themes/ThemeBuilder.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import ThemeBuilder from '$lib/themes/ThemeBuilder.svelte';
+</script>
+
+<Meta title="Themes/Builder" />
+
+<Template>
+	<ThemeBuilder />
+</Template>
+
+<Story name="Default"></Story>
---
+++ src/lib/themes/ThemeBuilder.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Select from '$lib/ui/Select.svelte';
+	import Textfield from '$lib/ui/Textfield.svelte';
+</script>
+
+<div class="flex flex-col gap-6">
+	<div><button class="btn btn-primary" type="button">Button</button></div>
+	<div><Textfield value="" /></div>
+	<div><Select options={['Apples', 'Bananas', 'Pineapples']} /></div>
+</div>
