cb8f6f8f2e364a866beb6be4ad88f2562e0d54c5
Jo Burgard <mail@joburgard.com>
2023-08-06 08:36:27 +0200

    Add: label component

---
+++ src/lib/ui/Label.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let text: string;
+</script>
+
+<label class="inline-flex flex-col">
+	<span class="font-semibold text-sm text-gray-600 tracking-wide">{text}</span>
+	<slot />
+</label>
