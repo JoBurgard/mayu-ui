4811b3483c8d4a5b58be5817c77f1727156f7b95
Jo Burgard <mail@joburgard.com>
2024-02-25 15:35:24 +0100

    fix(ContextMenu): add component to exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,4 +7,5 @@ export * from './ui/button';
 export * from './ui/input';
 export * from './ui/combobox';
 export * from './ui/tooltip';
+export * from './ui/contextMenu';
 // export * from './ui/select/';
