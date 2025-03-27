c815d3115cc4e83974f58741a1e55976d7746516
Jo Burgard <mail@joburgard.com>
2025-03-27 07:39:17 +0100

    fix(Dialog): z-index problem solved

--- src/lib/ui/dialog/Dialog.svelte
+++ src/lib/ui/dialog/Dialog.svelte
@@ -84,7 +84,7 @@ SPDX-License-Identifier: Unlicense
 </script>
 
 {#if open}
-	<div class="isolate fixed" use:melt={$portalled}>
+	<div class="isolate fixed z-[calc(infinity)]" use:melt={$portalled}>
 		<div
 			use:melt={$overlay}
 			class={dialogOverlayVariants({ class: overlayClasses })}
