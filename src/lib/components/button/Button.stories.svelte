29b7eb9f6259683fa65a8b0d69350538d14e6ecf
Jo Burgard <mail@joburgard.com>
2025-04-19 07:58:13 +0200

    initial setup

---
+++ src/lib/components/button/Button.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'Basics/Button'
+	});
+</script>
+
+<script lang="ts"></script>
+
+<Story name="Default">
+	<button type="button" class="btn">Button text</button>
+</Story>
